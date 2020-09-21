78c3bca2ec2fdfa120a304e8ee8e8ef97f1164c9
Josef Kemetmüller <josef.kemetmueller@ims.co.at>
2020-09-21 21:29:27 +0200

    Refactor default.nix

--- default.nix
+++ default.nix
@@ -1,19 +1,16 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let
   inherit (nixpkgs) pkgs;
-
-  nixPackages = [
-    pkgs.nodejs-10_x
-    pkgs.yarn
-  ];
 in
 pkgs.stdenv.mkDerivation rec {
   name = "vscode-env-selector";
-  buildInputs = nixPackages;
-  postInstall =
-    ''
-      yarn install
-      export PATH="$PWD/node_modules/.bin/:$PATH"
-    '';
+  buildInputs = with pkgs; [
+    nodejs
+    yarn
+  ];
+  postInstall = ''
+    yarn install
+    export PATH="$(yarn bin):$PATH"
+  '';
   shellHook = postInstall;
 }
